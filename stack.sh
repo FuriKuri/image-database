a52ba2cbd535f7bbf09e1738c0756f044f5233d6
Theo Pack Test <tf.pack@gmail.com>
2017-05-07 14:54:37 +0200

    Add outputs

--- stack.sh
+++ stack.sh
@@ -12,4 +12,10 @@ else
     aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name ${STACKNAME} --template-body file://cfn.yml || echo "No Update"
 fi
 
-cfn-tail ${STACKNAME}
+echo "Wait for stack complete ..."
+
+aws cloudformation wait stack-create-complete --stack-name ${STACKNAME}
+
+PAGE_URL=$(aws cloudformation describe-stacks --stack-name ${STACKNAME} | jq '.Stacks[0].Outputs[] | select(.OutputKey=="PageUrl") | .OutputValue')
+
+echo "Search images over: $PAGE_URL"
