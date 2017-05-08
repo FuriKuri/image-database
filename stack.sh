095cd80185c59bbf2c5dd5dfb0ef46fb6edf11b0
Theo Pack Test <tf.pack@gmail.com>
2017-05-08 18:58:14 +0200

    Write stderr to dev/null

--- stack.sh
+++ stack.sh
@@ -6,7 +6,7 @@ export AWS_DEFAULT_REGION=eu-west-1
 
 STACKNAME=image-database
 
-if ! aws cloudformation describe-stacks --stack-name ${STACKNAME} > /dev/null; then
+if ! aws cloudformation describe-stacks --stack-name ${STACKNAME} &> /dev/null; then
     aws cloudformation create-stack --capabilities CAPABILITY_IAM --stack-name ${STACKNAME} --template-body file://cfn.yml
 else
     aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name ${STACKNAME} --template-body file://cfn.yml || echo "No Update"
